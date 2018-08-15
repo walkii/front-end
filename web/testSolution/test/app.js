c9f5b7e544b18cbaa80a7d77823b943524e4751c
walkii <julien_denoulet@orange.fr>
2018-08-15 09:03:08 +0100

    add ES6

--- web/testSolution/test/app.js
+++ web/testSolution/test/app.js
@@ -1,4 +1,5 @@
-﻿document.addEventListener(
+﻿//import { myTimer, openCity} from "./appExport.js";
+document.addEventListener(
     "DOMContentLoaded",
     function () {
         var app = new Vue({
@@ -32,7 +33,7 @@ function openCity(evt, cityName) {
     // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
-}
+};
 
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
@@ -45,16 +46,16 @@ var x = setInterval(function () {
 
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+     // Time calculations for days, hours, minutes and seconds
+    const date = {
+         day: Math.floor(distance / (1000 * 60 * 60 * 24)),
+         hour: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+         minute: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+         second: Math.floor((distance % (1000 * 60)) / 1000)
+    }
+    let { day, hour, minute, second } = date;
     // Display the result in the element with id="demo"
-    document.getElementById("timer").innerHTML ="Next : " + days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    document.getElementById("timer").innerHTML = myTimer(day, hour, minute, second);
 
     // If the count down is finished, write some text 
     if (distance < 0) {
@@ -62,3 +63,15 @@ var x = setInterval(function () {
         document.getElementById("timer").innerHTML = "EXPIRED";
     }
 }, 1000);
+
+function myTimer(days=2, hours, minutes, seconds) {
+    return `Next : ${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
